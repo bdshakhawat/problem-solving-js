ad858338034def5795af2e2d19309c59c468e0dc
bdshakhawat <mdshakwat.hossain@gmail.com>
2023-09-02 23:34:22 +0600

    Solution 2 commit

--- leetcode.js
+++ leetcode.js
@@ -1,4 +1,6 @@
-// You are given two strings word1 and word2. Merge the strings by adding letters in alternating order, 
+//Problem:1
+
+//  You are given two strings word1 and word2. Merge the strings by adding letters in alternating order, 
 // starting with word1. If a string is longer than the other, append the additional letters onto the end of 
 // the merged string.
 
@@ -37,6 +39,9 @@
 
 // Solution:
 
+
+// 
+
 // write a function called mergeAlternating that takes two string parameters: word1 and word2
 function mergeAlternating(word1,word2){
 // Declare a variable 'merged' and initialize it as an empty string. 
@@ -70,3 +75,68 @@ let test1 = mergeAlternating("abc", "pqr");   // Expected output: "apbqcr"
 let test2 = mergeAlternating("ab", "pqrs");   // Expected output: "apbqrs"
 let test3 = mergeAlternating("abcd", "pq");   // Expected output: "apbqcd"
 
+
+
+
+// Problem:2
+
+// For two strings s and t, we say "t divides s" if and only if s = t + ... + t (i.e., t is concatenated with itself one or more times).
+
+// Given two strings str1 and str2, return the largest string x such that x divides both str1 and str2.
+
+ 
+
+// Example 1:
+
+// Input: str1 = "ABCABC", str2 = "ABC"
+// Output: "ABC"
+// Example 2:
+
+// Input: str1 = "ABABAB", str2 = "ABAB"
+// Output: "AB"
+// Example 3:
+
+// Input: str1 = "LEET", str2 = "CODE"
+// Output: ""
+ 
+
+// Constraints:
+
+// 1 <= str1.length, str2.length <= 1000
+// str1 and str2 consist of English uppercase letters.
+
+// Solution:
+// Write a helper function named gcd which calculates the greatest common divisor (GCD) of two numbers, 
+// a and b, using the Euclidean algorithm:
+function gcd(a, b) {
+  // If b is zero, the function returns a .
+  if (b === 0) return a;
+  // Otherwise, the function calls itself recursively with b and the remainder of a divided by b.
+  return gcd(b, a % b);
+}
+
+
+// Write  the main function to determine the largest common substring for the given strings str1 and str2.
+
+// The condition (str1 + str2 !== str2 + str1) checks if the concatenation of str1 and str2 is different from the concatenation of str2 and str1. If they are different, it means the strings don't have the same repeating pattern. In this case, the function returns an empty string.
+// The line const len = gcd(str1.length, str2.length); calculates the GCD of the lengths of str1 and str2 using the previously defined gcd function. The reasoning behind this is that the length of the common substring that divides both strings must be a divisor of the lengths of both strings.
+// The line return str1.substring(0, len); returns a substring of str1 that starts at the beginning (index 0) and has a length of len. This substring is the largest common substring that divides both str1 and str2
+function findLargestCommonSubstring(str1, str2) {
+  // Check if both strings are the sequences of copies of a common substring
+  if (str1 + str2 !== str2 + str1) return ""; 
+
+  const len = gcd(str1.length, str2.length);
+  return str1.substring(0, len);
+}
+
+// This is an example usage of the findLargestCommonSubstring function:
+
+// Two strings, str1 and str2, are initialized.
+// The result of the function is then printed to the console using console.log.
+
+const str1 = "ABABAB";
+const str2 = "ABAB";
+console.log(findLargestCommonSubstring(str1, str2));  
+
+// PS E:\problem_solving-leetcode-hackerrank\problem-solving-js> node leetcode.js
+// AB
